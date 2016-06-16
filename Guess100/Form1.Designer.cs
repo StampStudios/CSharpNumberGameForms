80106a3d094ffabd0d489133d521a3b52ade7caf
ItsTenchy <jack@jtench.co.uk>
2016-06-16 14:01:51 +0100

    Moved live counter.

--- Guess100/Form1.Designer.cs
+++ Guess100/Form1.Designer.cs
@@ -84,7 +84,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(175, 52);
+            this.label3.Location = new System.Drawing.Point(190, 13);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 5;
